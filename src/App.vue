6c24ceb12531a020cd2b7c3beea66e91da052fcc
Marius <grausas@gmail.com>
2020-09-17 19:42:52 +0300

    Added navigation

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <Navigation />
+    <div class="section">
+      <div class="container">
+        <router-view />
+      </div>
     </div>
-    <router-view />
   </div>
 </template>
+
+<script>
+import Navigation from "./components/Navigation";
+
+export default {
+  components: { Navigation },
+};
+</script>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,39 @@
+<template>
+  <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
+    <div class="container">
+      <div id="navbarBasicExample" class="navbar-menu">
+        <div class="navbar-start">
+          <router-link
+            class="navbar-item"
+            v-for="link in links"
+            :key="link.url"
+            :to="link.url"
+          >
+            {{ link.name }}
+          </router-link>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      links: [
+        { name: "View Groups", url: "/viewgroups" },
+        { name: "Add Group", url: "/addgroup" },
+        { name: "Fill Journal", url: "/filljournal" },
+        { name: "Edit group", url: "/editgroup" },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.navbar-item:first-child {
+  padding-left: 0;
+}
+</style>
