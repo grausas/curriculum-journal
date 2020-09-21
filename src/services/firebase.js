597c5e4d9dfb5df3af63be4de211815463cbfe13
Marius <grausas@gmail.com>
2020-09-21 19:03:02 +0300

    firebase config added

---
+++ src/services/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBeDug4ipBec4eCuuEjuAAND0KVIgWrPfo",
+  authDomain: "dev-curriculum-journal.firebaseapp.com",
+  databaseURL: "https://dev-curriculum-journal.firebaseio.com",
+  projectId: "dev-curriculum-journal",
+  storageBucket: "dev-curriculum-journal.appspot.com",
+  messagingSenderId: "924999135957",
+  appId: "1:924999135957:web:2e9a51cba681c29d75285c",
+};
+
+firebase.initializeApp(firebaseConfig);
