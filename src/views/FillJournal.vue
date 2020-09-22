8233acb7efa6300dadbab5f319e09e64050fdb5d
Marius <grausas@gmail.com>
2020-09-22 18:53:04 +0300

    added functionality for fill journal

--- src/views/FillJournal.vue
+++ src/views/FillJournal.vue
@@ -1,16 +1,23 @@
 <template>
   <div class="fill-journal ">
+    <Notification
+      v-if="error"
+      v-on:close="error = false"
+      :type="errorType"
+      :message="errorMessage"
+    />
     <div class="container">
       <h1 class="subtitle">Fill Group</h1>
-      <form>
+      <form v-on:submit.prevent="filljournal">
         <div class="field">
           <label class="label">Group name: </label>
           <div class="control">
             <div class="select">
-              <select>
-                <option>Select Group</option>
-                <option>Example 1</option>
-                <option>Example 2</option>
+              <select v-model="selectedGroup">
+                <option value="" disabled>Select group</option>
+                <option v-for="groupList in groupsList" :key="groupList.id">
+                  {{ groupList.gname }}
+                </option>
               </select>
             </div>
           </div>
@@ -19,7 +26,12 @@
         <div class="field">
           <label class="label">Date: </label>
           <div class="control">
-            <input class="input" type="date" placeholder="Date" />
+            <input
+              class="input"
+              type="date"
+              v-model="date"
+              placeholder="Date"
+            />
           </div>
         </div>
 
@@ -29,6 +41,7 @@
             <input
               class="input"
               type="number"
+              v-model="childrenattended"
               placeholder="Children attended"
             />
           </div>
@@ -41,6 +54,7 @@
               class="textarea has-fixed-size"
               placeholder="Extra info..."
               rows="2"
+              v-model="extra"
             >
             </textarea>
           </div>
@@ -59,8 +73,65 @@
 </template>
 
 <script>
+import firebase from "firebase/app";
+import "firebase/firestore";
+import Notification from "../components/Notification";
+
 export default {
   name: "FillJournal",
+  components: { Notification },
+  data() {
+    return {
+      selectedGroup: [],
+      date: "",
+      childrenattended: "",
+      extra: "",
+      groupsList: [],
+      error: false,
+      errorType: "",
+      errorMessage: "",
+    };
+  },
+  methods: {
+    filljournal() {
+      firebase
+        .firestore()
+        .collection("journal")
+        .add({
+          selectedGroup: this.selectedGroup,
+          childrenattended: this.childrenattended,
+          date: this.date,
+          extra: this.extra,
+        })
+        .then(
+          () => {
+            this.error = true;
+            this.errorType = "is-success";
+            this.errorMessage = "You have successfully added a group";
+          },
+          (error) => {
+            this.error = true;
+            this.errorType = "is-danger";
+            this.errorMessage = error.message;
+          }
+        );
+    },
+  },
+  beforeMount() {
+    firebase
+      .firestore()
+      .collection("groups")
+      .get()
+      .then((snapshot) => {
+        snapshot.docs.forEach((doc) => {
+          this.groupsList.push({
+            id: doc.id,
+            gname: doc.data().gname,
+            children: doc.data().children,
+          });
+        });
+      });
+  },
 };
 </script>
 
