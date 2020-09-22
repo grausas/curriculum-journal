615a5254cb8f9a74213712399a1193758929817d
Marius <grausas@gmail.com>
2020-09-22 11:26:56 +0300

    Added add group functionality

--- src/views/AddGroup.vue
+++ src/views/AddGroup.vue
@@ -1,32 +1,56 @@
 <template>
   <div class="add-group ">
+    <Notification
+      v-if="error"
+      v-on:close="error = false"
+      :type="errorType"
+      :message="errorMessage"
+    />
     <div class="container">
       <h1 class="subtitle">Add Group</h1>
-      <form>
+      <form v-on:submit.prevent="addGroup">
         <div class="field">
           <label class="label">Group name: </label>
           <div class="control">
-            <input class="input " type="text" placeholder="Group name" />
+            <input
+              class="input "
+              id="gname"
+              type="text"
+              v-model="gname"
+              placeholder="Group name"
+            />
           </div>
         </div>
 
         <div class="field">
           <label class="label">Children in group: </label>
           <div class="control">
-            <input class="input" type="number" placeholder="Childrens" />
+            <input
+              class="input"
+              id="children"
+              type="number"
+              v-model="children"
+              placeholder="Childrens"
+            />
           </div>
         </div>
 
         <div class="field">
           <label class="label">Distance to group: </label>
           <div class="control">
-            <input class="input" type="number" placeholder="Distance (km)" />
+            <input
+              class="input"
+              id="distance"
+              type="number"
+              v-model="distance"
+              placeholder="Distance (km)"
+            />
           </div>
         </div>
 
         <div class="field">
           <div class="control">
-            <button class="button is-success">
+            <button class="button is-success" type="submit">
               <span>Add Group</span>
             </button>
           </div>
@@ -37,8 +61,47 @@
 </template>
 
 <script>
+import firebase from "firebase/app";
+import "firebase/firestore";
+import Notification from "../components/Notification";
+
 export default {
   name: "AddGroup",
+  components: { Notification },
+  data() {
+    return {
+      gname: "",
+      children: "",
+      distance: "",
+      error: false,
+      errorType: "",
+      errorMessage: "",
+    };
+  },
+  methods: {
+    addGroup() {
+      firebase
+        .firestore()
+        .collection("groups")
+        .add({
+          gname: this.gname,
+          children: this.children,
+          distance: this.distance,
+        })
+        .then(
+          () => {
+            this.error = true;
+            this.errorType = "is-success";
+            this.errorMessage = "You have successfully added a group";
+          },
+          (error) => {
+            this.error = true;
+            this.errorType = "is-danger";
+            this.errorMessage = error.message;
+          }
+        );
+    },
+  },
 };
 </script>
 
