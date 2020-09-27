2b2fb06dffa8332d1f9136a2c45f9cade5b171b8
Marius <grausas@gmail.com>
2020-09-27 22:08:30 +0300

    added view journal mobile table

--- src/views/ViewJournal.vue
+++ src/views/ViewJournal.vue
@@ -12,9 +12,9 @@
       </div>
     </div>
     <button class="button is-small is-success" @click="sortBy()">
-      Sort
+      Sort by date
     </button>
-    <table class="table is-striped is-narrow ">
+    <table class="table is-striped is-narrow">
       <thead>
         <tr>
           <th>Date</th>
@@ -26,8 +26,12 @@
           <th>Delete</th>
         </tr>
       </thead>
-      <tbody v-for="(journal, index) in filteredList" :key="index">
-        <tr v-bind="journal in journals" :key="journal.id">
+      <tbody>
+        <tr
+          v-for="(journal, index) in filteredList"
+          :key="index"
+          v-bind="journal in journals"
+        >
           <td>{{ journal.date }}</td>
           <td>{{ journal.gname }}</td>
           <td>{{ journal.children }}</td>
@@ -79,13 +83,13 @@ export default {
     },
     sortBy() {
       let assending = false;
-      this.journals = this.journals.sort((x, y) => {
+      this.journals.sort((x, y) => {
         let a = x.gname.toLowerCase();
         let b = y.gname.toLowerCase();
         if (assending) {
-          return a > b ? 1 : -1;
+          return a < b ? 1 : -1;
         } else {
-          return a > b ? -1 : 1;
+          return a < b ? -1 : 1;
         }
       });
       console.log(assending);
@@ -111,21 +115,97 @@ export default {
             extra: doc.data().extra,
           });
         })
-      );
+      )
+      .then(() => {
+        this.journals.sort((a, b) => (a.gname > b.gname ? -1 : 1));
+      });
   },
 };
 </script>
 
 <style scoped>
+table {
+  margin-top: 10px;
+}
+
 table > tbody > tr > td:last-child {
   text-align: center;
 }
 
 input {
+  width: 100%;
   max-width: 30%;
 }
 
 input:focus {
   background-color: rgb(198, 237, 250);
+}
+
+@media only screen and (max-width: 850px) {
+  table,
+  thead,
+  tbody,
+  th,
+  td,
+  tr {
+    display: block;
+  }
+  thead tr {
+    position: absolute;
+    top: -9999px;
+    left: -9999px;
+  }
+  tr {
+    border: 1px solid #ccc;
+  }
+  td {
+    border: none;
+    border-bottom: 1px solid #eee;
+    position: relative;
+    padding-left: 200px;
+    margin-left: 150px;
+  }
+  td:before {
+    position: absolute;
+    left: 6px;
+    width: 200px;
+    padding-right: 40px;
+    white-space: nowrap;
+    margin-left: -150px;
+  }
+  td:nth-of-type(1):before {
+    content: "Date:";
+  }
+  td:nth-of-type(2):before {
+    content: "Group name:";
+  }
+  td:nth-of-type(3):before {
+    content: "Children in group:";
+  }
+  td:nth-of-type(4):before {
+    content: "Children attended:";
+  }
+  td:nth-of-type(5):before {
+    content: "Distance(km):";
+  }
+  td:nth-of-type(6):before {
+    content: "Extra info:";
+  }
+  td:nth-of-type(7):before {
+    content: "Delete:";
+  }
+
+  table > tbody > tr > td:last-child {
+    text-align: left;
+    column-span: all;
+  }
+
+  table > thead > tr > th:last-child {
+    text-align: left;
+  }
+
+  input {
+    max-width: 100%;
+  }
 }
 </style>
